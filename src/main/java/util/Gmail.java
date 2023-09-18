9cfad65041f0a1b42875768da1d8694801e1a1f0
Dachan Seo <111109411+DevDachan@users.noreply.github.com>
2023-09-18 12:42:24 +0900

    Update Gmail.java

--- src/main/java/util/Gmail.java
+++ src/main/java/util/Gmail.java
@@ -8,6 +8,6 @@ public class Gmail extends Authenticator{
 	// 해당 내용은 관리자 권한으로 사용자에게 보낼 메일을 위해 관리자 아이디와 비밀번호를 입력
 	// 보안에 문제가 되기 때문에 protected method type으로 보호해준다.
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("dachan9524@gmail.com","mjdppawewxspdnag");	
+		return new PasswordAuthentication("dachan9524@gmail.com","2차 인증 비밀번호");	
 	}	
 }
